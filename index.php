097e3306b8f2cd2e7cce46cca917470430adb5ac
Roetjan <93587343+Roetjan@users.noreply.github.com>
2022-03-30 16:13:06 +0200

    basic layout gemaakt

---
+++ index.php
@@ -0,0 +1,19 @@
+<html lang="nl" charset="UTF-8">
+<head>
+<meta>
+</head>
+
+</head>
+<body>
+    <header>
+
+    </header>
+
+    <main>
+        
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
