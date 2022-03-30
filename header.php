a155138e604eb7dcf26e7f13f8e5419846589b09
thomasbouman1505 <65914841+thomasbouman1505@users.noreply.github.com>
2022-03-30 16:25:12 +0200

    Update header.php

--- header.php
+++ header.php
@@ -2,8 +2,8 @@
 	<img src="" alt="Logo">
 	<p>Naam van het bedrijf eronder</p>
 	<nav>
-		<a href="">#</a>
-		<a href="">#</a>
+		<a href="contactpagina.php">contactpagina</a>
+		<a href="index.php">home</a>
 		<a href="">#</a>
 	</nav>
 </header>
