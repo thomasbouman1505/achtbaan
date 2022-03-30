b63ccee5cdf1314c4c28fe5d3b3d7440a32dcd56
Roetjan <93587343+Roetjan@users.noreply.github.com>
2022-03-30 16:15:25 +0200

    basic layout

--- index.php
+++ index.php
@@ -1,19 +1,12 @@
-<html lang="nl" charset="UTF-8">
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<meta>
-</head>
-
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 <body>
-    <header>
-
-    </header>
-
-    <main>
-        
-    </main>
-    <footer>
-
-    </footer>
+    
 </body>
 </html>
