1f44d7faf86dafd4083bee8ab8560a7ea1f586bc
Roetjan <93587343+Roetjan@users.noreply.github.com>
2022-04-04 10:52:13 +0200

    divs toegevoegd om te stylen

--- header.php
+++ header.php
@@ -1,9 +1,13 @@
 <header>
-	<img src="" alt="Logo">
-	<p>Naam van het bedrijf eronder</p>
-	<nav>
-		<a href="contactpagina.php">contactpagina</a>
-		<a href="index.php">home</a>
-		<a href="">#</a>
-	</nav>
+	<div class="logo">
+		<img src="" class= alt="Logo">
+		<p>Logo</p>
+	</div>
+	<div class="container">
+		<nav>
+			<a href="contactpagina.php">contactpagina</a>
+			<a href="index.php">home</a>
+			<a href="">#</a>
+		</nav>
+	</div>
 </header>
