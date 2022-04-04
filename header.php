b8817f8bfbe0b7212ee4343b4b4610b0b6a809c0
thomasbouman1505 <65914841+thomasbouman1505@users.noreply.github.com>
2022-04-04 10:57:11 +0200

    logo

--- header.php
+++ header.php
@@ -1,6 +1,9 @@
 <header>
-	<img src="" alt="Logo">
+	<div class="logo">
+	<img src="img/achtbaan.jpg" alt="Logo">
+</div>
 	<p>Naam van het bedrijf eronder</p>
+	
 	<nav>
 		<a href="contactpagina.php">contactpagina</a>
 		<a href="index.php">home</a>
