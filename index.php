cb9a6348e0aec8bbc15455816dd5e6d948a38317
thomasbouman1505 <65914841+thomasbouman1505@users.noreply.github.com>
2022-04-11 10:43:39 +0200

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,8 @@
 <body>
     <?php require_once('header.php') ?>
     <main>
-     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias consectetur deleniti soluta dolores architecto itaque asperiores saepe, eum id, inventore explicabo perferendis deserunt ducimus quis dicta sit. Excepturi, modi inventore.</p>
+        <h2>waarom onze achtbaantechniek?</h2>
+     <p>We hebben een nieuwe achtbaantechniek ontwikkeld en het heet magnadrive . we hebben een middel gevonden die ons team heeft uitgevonden . door deze techniek kunnen we een achtbaan produceren die tot wel 10% sneller kan en paar graden scherper door de bocht en hij gaat ook nog een langer mee . de achtbaan is ook nog eens goed voor de natuur want hij is voor een deel gemaakt van oude onderdelen en het het is niet eens duurde dan onze concurrenten en door onze techniek kunnen we de attractie veel compacter bouwen waardoor je meer ruimt over hebt voor andere dingen en het zal minder kosten </p>
     </main>
     <?php require_once('footer.php') ?>
 </body>
